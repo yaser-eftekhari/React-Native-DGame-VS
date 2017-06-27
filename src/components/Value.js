d156e9e4c86f70be6728b49932f56fde188f33c4
nesa.asoudeh <nesa.asoudeh@irdeto.com>
2017-06-27 13:44:02 -0400

    -Added picker to value

--- src/components/Value.js
+++ src/components/Value.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Picker} from 'react-native';
 
-const Value = (props) => {
-  const { textStyle } = styles;
-  return (
-    <View style={props.look}>
-      <Text style={textStyle}> Value </Text>
-    </View>
-  );
-};
+class Value extends Component {
 
-const styles = {
-  textStyle: {
-    fontSize: 20
+  state = { language: [] };
+  //const { textStyle } = styles;
+  render(){
+    return (
+      <View> 
+          <Picker
+            selectedValue={this.state.language}
+            onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+            <Picker.Item label="Accountability" value="acc" />
+            <Picker.Item label="Agility" value="ag" />
+            <Picker.Item label="Innovation" value="in" />
+            <Picker.Item label="Trust" value="tr" />
+          </Picker>
+      </View>
+    );
   }
-};
+}
+
+/*const styles = {
+  textStyle: {
+    fontSize: 15,
+    alignSelf: 'center'
+  },
+  langContainer: {
+    flex: 1,
+    backgroundColor: '#F8F8F8',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    elevation: 2,
+    position: 'relative',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  border:{
+    flex: 1,
+    flexDirection: 'row',
+    position: 'relative',
+    justifyContent: 'center'
+  },
+  pickerContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+}; */
 
 export default Value;
